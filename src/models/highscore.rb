78f39a0abbf02abf6e4828abcac1bdef89bba157
kesleymartins <kesley.martins@vivaldi.net>
2023-01-21 17:02:37 -0300

    create highscore modal

---
+++ src/models/highscore.rb
@@ -0,0 +1,11 @@
+class Highscore
+    include Mongoid::Document
+
+    field :username, type: String
+    field :score, type: Integer
+    field :game, type: String
+
+    validates :username, presence: true
+    validates :score, presence: true
+    validates :game, presence: true
+end
