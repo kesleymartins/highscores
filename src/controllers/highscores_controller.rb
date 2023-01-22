806fd81462263fac6900386b71ca62d629e460fc
kesleymartins <kesley.martins@vivaldi.net>
2023-01-22 01:16:56 -0300

    return highscore list to template

--- src/controllers/highscores_controller.rb
+++ src/controllers/highscores_controller.rb
@@ -1,2 +1,13 @@
 class HighscoresController < ApplicationController
+    helpers Sinatra::ContentFor
+
+    configure do
+        set :views, "src/views/highscores"
+    end
+
+    get '/highscores' do
+        @highscores = Highscore.all
+
+        slim :index, layout: layout_path('base')
+    end
 end
