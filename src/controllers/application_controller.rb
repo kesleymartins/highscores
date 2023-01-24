04c8db4487f9ef1dd68528518953442fdba1c049
kesleymartins <kesley.martins@vivaldi.net>
2023-01-24 00:51:35 -0300

    add current user

--- src/controllers/application_controller.rb
+++ src/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < Sinatra::Base
         enable :sessions
     end
 
+    before do
+        @current_user = User.where(id: session[:user_id]).first
+    end
+
     def layout_path(name)
         "../layout/#{name}".to_sym
     end
