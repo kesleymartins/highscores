3721f0b14b9913e7c204d90094310c399d8c85c9
kesleymartins <kesley.martins@vivaldi.net>
2023-01-27 00:08:00 -0300

    remove bcrypt logic

--- src/models/user.rb
+++ src/models/user.rb
@@ -3,18 +3,7 @@ class User
     include BCrypt
 
     field :username, type: String
-    field :password_hash, type: String
     
     validates :username, presence: true
-    validates :password_hash, presence: true
     validates_uniqueness_of :username
-
-    def password
-        @password ||= Password.new(password_hash)
-    end
-    
-    def password=(new_password)
-        @password = Password.create(new_password)
-        self.password_hash = @password
-    end
 end
