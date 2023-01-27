432b0d0586cf9e7ec5386fd7cf2d2a2707a1d5e2
kesleymartins <kesley.martins@vivaldi.net>
2023-01-27 00:09:35 -0300

    hash password before create

--- src/models/user.rb
+++ src/models/user.rb
@@ -8,4 +8,12 @@ class User
     validates :username, presence: true
     validates :password, presence: true, confirmation: true
     validates_uniqueness_of :username
+
+    before_create :hash_password
+
+    protected
+
+    def hash_password
+        self.password = Argon2::Password.create(self.password)
+    end
 end
